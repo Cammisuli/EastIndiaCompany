9fa55cf5b65251bd3c78e2752739eb7b6dbf844e
Jonathan Cammisuli <jonathan.cammisuli@gmail.com>
2015-04-20 08:46:44 -0400

    Added bundle config and begin rework

---
+++ EastIndiaCompanyUmbraco/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace EastIndiaCompanyUmbraco.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/sitecss")
+                .Include("~/App_Plugins/Merchello.Bazaar/Themes/EastIndiaCompany/Assets/css/bootstrap.css")
+                .Include("~/App_Plugins/Merchello.Bazaar/Themes/EastIndiaCompany/Assets/css/styles.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/sitejs")
+                .Include("~/App_Plugins/Merchello.Bazaar/Themes/EastIndiaCompany/Assets/js/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/corejs")
+                .Include("~/App_Plugins/Merchello.Bazaar/Assets/js/jquery.unobtrusive-ajax.min.js")
+                .Include("~/App_Plugins/Merchello.Bazaar/Assets/js/jquery.validate.unobtrusive.min.js")
+                .Include("~/App_Plugins/Merchello.Bazaar/Assets/js/jquery.validate.min.js")
+                .Include("~/App_Plugins/Merchello.Bazaar/Assets/js/merchello.bazaar.js"));
+        }
+    }
+}
---
+++ EastIndiaCompanyUmbraco/Global.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using EastIndiaCompanyUmbraco.App_Start;
+using Umbraco.Web;
+
+namespace EastIndiaCompanyUmbraco
+{
+    public class Global : UmbracoApplication
+    {
+        protected override void OnApplicationStarted(object sender, EventArgs e)
+        {
+            base.OnApplicationStarted(sender, e);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
