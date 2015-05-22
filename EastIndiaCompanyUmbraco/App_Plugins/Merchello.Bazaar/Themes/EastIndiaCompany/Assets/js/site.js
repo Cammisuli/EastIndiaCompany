48d19aa9754e72b3dc9b79f9b6d19f855c53e468
Jonathan Cammisuli <jonathan.cammisuli@gmail.com>
2015-05-22 08:49:35 -0400

    fixed up mobile navigation

--- EastIndiaCompanyUmbraco/App_Plugins/Merchello.Bazaar/Themes/EastIndiaCompany/Assets/js/site.js
+++ EastIndiaCompanyUmbraco/App_Plugins/Merchello.Bazaar/Themes/EastIndiaCompany/Assets/js/site.js
@@ -1,7 +1,35 @@
-﻿$(function () {
+﻿function setTranslate(ele, value) {
+    ele.css("-webkit-transform", "translate3d(" + value + "px,0,0)");
+    ele.css("-moz-transform", "translate3d(" + value + "px,0,0)");
+    ele.css("-ms-transform", "translate3d(" + value + "px,0,0)");
+    ele.css("transform", "translate3d(" + value + "px,0,0)");
+}
+
+$(function () {
     //$("li.has-dropdown").mouseover(function () {
     //    var self = $(this);
 
     //    self.children("ul.dropdown").slideToggle();
     //});
+
+
+    // mobile navigation
+    var nav = $(".nav-items");
+    var navwidth = nav.width();
+    setTranslate(nav, -navwidth - 32);
+    setTimeout(function() {
+        nav.css("visibility", "visible");
+    }, 300);
+    var open = false;
+    $(".lines-button").click(function () {
+        $(this).toggleClass("ani");
+        if (open) {
+            setTranslate(nav, -navwidth - 32);
+            open = false;
+        } else {
+            setTranslate(nav, 0);
+            open = true;
+        }
+    });
+    
 });
